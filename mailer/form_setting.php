c407a5ae8d36674244c9deab5337aa98cf57a039
Antoine Foggs <fireheartfoggs@gmail.com>
2018-11-03 11:58:14 -0500

    Reset and recreated private email server. Set up email to work with php mailer.

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -2,7 +2,7 @@
 	/*Form settings*/
 	$subj = "New message from your personal Website"; //letter subject
 	$to = 'antoine.foggs@gmail.com'; // Enter Your E-mail
-	$from = 'afoggs@antoinefoggs.me'; // Admin e-mail
+	$from = 'contact@antoinefoggs.me'; // Admin e-mail
 	$fromName = 'Foggs Freelance'; // Your company name
 	$charset = 'UTF-8';
 ?>
