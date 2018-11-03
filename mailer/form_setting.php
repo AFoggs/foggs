6577fc48729fdb1b06fab92a1319e56fd379b8d6
Antoine Foggs <fireheartfoggs@gmail.com>
2018-11-02 22:24:59 -0500

    Used a template to jump-start a design for a representative personal website

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from your personal Website"; //letter subject
+	$to = 'antoine.foggs@gmail.com'; // Enter Your E-mail
+	$from = 'afoggs@antoinefoggs.me'; // Admin e-mail
+	$fromName = 'Foggs Freelance'; // Your company name
+	$charset = 'UTF-8';
+?>
