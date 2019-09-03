6a6ffd4bd5731309ad6f8ff632a40d1f84098e1c
Afoggs <fireheartfoggs@gmail.com>
2019-09-03 16:17:35 -0500

    Updated bio, removed push alert.

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -2,7 +2,7 @@
 	/*Form settings*/
 	$subj = "New message from your personal Website"; //letter subject
 	$to = 'antoine.foggs@gmail.com'; // Enter Your E-mail
-	$from = 'contact@antoinefoggs.me'; // Admin e-mail
+	$from = 'contact@afoggs.com'; // Admin e-mail
 	$fromName = 'Foggs Freelance'; // Your company name
 	$charset = 'UTF-8';
 ?>
